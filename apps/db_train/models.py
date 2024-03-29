1006157b40279f91ad55652e6946ede763faf0e3
konusa <u-kostya@mail.ru>
2024-03-29 21:10:03 +0300

    init1

--- apps/db_train/models.py
+++ apps/db_train/models.py
@@ -115,37 +115,35 @@ class Author(models.Model):
             self.age = today.year - self.date_birth.year - additional_year  # Перезаписываем значение
         super().save(*args, **kwargs)
 
-
     class Meta:
         verbose_name = "Автор"
         verbose_name_plural = "Авторы"
 
-# class Author(models.Model):
-#     phone_regex = RegexValidator(
-#         regex=r'^\+79\d{9}$',
-#         message="Телефонный номер должен быть формата: '+79123456789'."
-#     )
-#
-#     username = models.SlugField()
-#     email = models.EmailField()
-#     first_name = models.CharField(max_length=100)
-#     last_name = models.CharField(max_length=100)
-#     middle_name = models.CharField(max_length=100)
-#     gender = models.CharField(max_length=1, choices=[('м', 'мужской'), ('ж', 'женский')])
-#     self_esteem = models.DecimalField(max_digits=2, decimal_places=1)
-#     phone_number = models.CharField(max_length=12, validators=[phone_regex])
-#     phone_number = models.CharField(max_length=12)
-#     city = models.CharField(max_length=100)
-#     bio = models.TextField()
-#     age = models.IntegerField(null=True, editable=False)
-#     date_birth = models.DateField()
-#     status_rule = models.BooleanField()
-#     image = models.ImageField(upload_to='foto_profile')
-#     create_at = models.DateTimeField(auto_now_add=True)
-#     update_at = models.DateTimeField(auto_now=True)
-#
-#     def __str__(self):
-#         initials = None  # Инициалы
-#         if self.first_name and self.middle_name:
-#             initials = f"{self.first_name.upper()[0]}.{self.middle_name.upper()[0]}."
-#         return f"{self.username} - {self.last_name} {initials}"
+
+class AuthorProfile(models.Model):
+    author = models.OneToOneField('Author', on_delete=models.CASCADE)
+    stage = models.IntegerField(default=0,
+                                blank=True,
+                                verbose_name="Стаж",
+                                help_text="Стаж в годах")
+
+    def __str__(self):
+        return f"Автор: {self.author.username}; Стаж: {self.stage} лет"
+
+class Entry(models.Model):
+    text = models.TextField(verbose_name="Текст статьи",
+                            )
+    author = models.ForeignKey("Author", on_delete=models.CASCADE, related_name='entries')
+    tags = models.ManyToManyField("Tag", related_name='entries')
+
+    def __str__(self):
+        return f"Текст: {self.text.split()[0]}; Автор: {self.author.username}; Тэги: {self.tags.all().values_list('name')}"
+class Tag(models.Model):
+    name = models.CharField(max_length=50,
+                            verbose_name="Название",
+                            )
+    def __str__(self):
+        return f"Название Тэга: {self.name}"
+
+
+
