37d9aaa50f602b872e39120a504ce8d3beaa2b3d
konusa <u-kostya@mail.ru>
2024-04-07 23:21:16 +0300

    init3

--- apps/db_train/views.py
+++ apps/db_train/views.py
@@ -2,21 +2,32 @@ from django.shortcuts import render
 from django.views import View
 from .models import Author, AuthorProfile, Entry, Tag
 from django.db.models import Q, Max, Min, Avg, Count
+from django.db.models import F
 
 
 class TrainView(View):
     def get(self, request):
         max_self_esteem = Author.objects.aggregate(max_self_esteem=Max('self_esteem'))
         self.answer1 = Author.objects.filter(self_esteem=max_self_esteem['max_self_esteem'])  # TODO Какие авторы имеют самую высокую уровень самооценки(self_esteem)?
-        self.answer2 = None  # TODO Какой автор имеет наибольшее количество опубликованных статей?
-        self.answer3 = None  # TODO Какие статьи содержат тег 'Кино' или 'Музыка' ?
-        self.answer4 = None  # TODO Сколько авторов женского пола зарегистрировано в системе?
-        self.answer5 = None  # TODO Какой процент авторов согласился с правилами при регистрации?
-        self.answer6 = None  # TODO Какие авторы имеют стаж от 1 до 5 лет?
-        self.answer7 = None  # TODO Какой автор имеет наибольший возраст?
-        self.answer8 = None  # TODO Сколько авторов указали свой номер телефона?
-        self.answer9 = None  # TODO Какие авторы имеют возраст младше 25 лет?
-        self.answer10 = None  # TODO Сколько статей написано каждым автором?
+        #max_entry_authors = Entry.objects.aggregate(max_entry_authors=Max('author'))
+        #max_count_entry = Entry.objects.aggregate(max_count_entry=Max(Count('text')))
+        self.answer2 = Author.objects.annotate(num_entries=Count('entries')).order_by('-num_entries')[:1]
+        #self.answer2 = Entry.objects.filter(author=max_entry_authors['max_entry_authors'])     #TODO Какой автор имеет наибольшее количество опубликованных статей?
+        self.answer3 = Entry.objects.filter(Q(tags__name='Кино') | Q(tags__name='Музыка')).distinct()  # TODO Какие статьи содержат тег 'Кино' или 'Музыка' ?
+        self.answer4 = Author.objects.filter(gender='ж').count()# TODO Сколько авторов женского пола зарегистрировано в системе?
+        self.answer5 = round(Author.objects.filter(status_rule=True).count()*100/Author.objects.count(), 2)      #TODO Какой процент авторов согласился с правилами при регистрации?
+        self.answer6 = Author.objects.filter(authorprofile__stage__range=(1, 5))     #TODO Какие авторы имеют стаж от 1 до 5 лет?
+        max_age = Author.objects.aggregate(Max('age'))
+        self.answer7 = Author.objects.get(age=max_age['age__max'])# TODO Какой автор имеет наибольший возраст?
+        self.answer8 = Author.objects.filter(phone_number__isnull=False).count()  # TODO Сколько авторов указали свой номер телефона?
+        self.answer9 = Author.objects.filter(age__lt=25)  # TODO Какие авторы имеют возраст младше 25 лет?
+        self.answer10 = Entry.objects.values('author__username').annotate(count=Count('id'),
+                                                                          username=F('author__username'))  # TODO Сколько статей написано каждым автором?
         context = {f'answer{index}': self.__dict__[f'answer{index}'] for index in range(1, 11)}
 
         return render(request, 'train_db/training_db.html', context=context)
+
+
+if __name__ == "__main__":
+
+    print(TrainView())
